8af15a849ccbb497ba645a366363ace197952708
Talen-520 <63370853+Talen-520@users.noreply.github.com>
2022-02-09 17:33:19 -0500

    Create ADTofImplementation.cpp

---
+++ ADTofImplementation.cpp
@@ -0,0 +1,20 @@
+//Linked-list implementation of Stack, Queue, and ordered list.
+/*
+What your program need to do as follow:
+1) Build a stack: a) open an input file; b) read one data at the time from the input file; c) create a new node with data; d)
+push (newNode), on top of the stack.
+2) Build a queue: a) pop the stack; b) print the data in the node to outFile1, c) insertQ (node), at the back of the queue.
+3) Build a list: a) delete a node from the front of the queue; b) print the data in the node to outFile2, c) insertLL (node) to
+the list, in ascending order.
+4) Print list: a) print the entire list from the beginning to the end of the list to outFile3.
+Note: You must use argv to open input file and 3 output files. -5 points deduction if you hard-code your file names!
+*/
+
+#include <fstream>
+#include <iostream>
+
+int main(){
+  struct(){
+  }
+
+}
